aa9c0b15870bec2a23a2c0ca8bbc23ad1e4e4c24
ifbalta <cheska.baltazar@gmail.com>
2015-07-03 15:58:35 +1200

    Added post action to form. Need to implement post method next

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var app = express();
 
+var mongo = require('mongodb');
+
 app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 3000);
+console.log("started server");
