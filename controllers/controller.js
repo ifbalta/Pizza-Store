ce2b97e827060a4eb4102eb4ca5365f8f2fa7851
Isabel Francesca Baltazar <cheska.baltazar@gmail.com>
2015-08-26 12:01:58 +1200

    Added checkbox controllers using checklist-model. My dependency list is getting longer and longer.

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,27 +1,38 @@
 /**
  * Angular.js controller
  */
-var orderApp = angular.module('orderApp', []);
+
+var orderApp = angular.module('orderApp', ['checklist-model']);
 
 orderApp.controller ('displayCtrl', ['$scope', '$http', function ($scope, $http) {
     console.log("displayCtrl is listening...");
 
+    $scope.criteria = [
+        'City',
+        'Pizza Type',
+        'Completed',
+        'Order Date'
+    ];
+
+    $scope.query = {
+        criteria : []
+    };
+
     var refresh = function () {
         $http.get('/allOrders').success(function (response) {
             console.log("Retrieved orders");
-            console.log(response[0].customerName);
             $scope.orderlist = response;
-            console.log($scope.orderlist[0].customerAddress);
         });
-    }
+    };
     refresh();
 
     $scope.fillTable = function () {
         console.log("Where are my queries?");
-    }
+        console.log($scope.query.criteria)
+    };
 
     $scope.reload = function () {
         console.log("Reloading page");
         refresh();
-    }
+    };
 }]);
